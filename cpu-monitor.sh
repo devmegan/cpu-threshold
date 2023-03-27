093aea3046bb4f699060ced4434af25b1dc55802
devmegan <devmegan@protonmail.com>
2023-03-27 20:21:05 +0100

    add: write title and timestamp to logfile

--- cpu-monitor.sh
+++ cpu-monitor.sh
@@ -5,6 +5,10 @@ program_name=$1
 echo "CPU threshold monitor for $program_name running"
 echo
 
+echo "CPU threshold monitor for $program_name [$(date '+%Y-%m-%d %H:%M:%S')]:" >> logfile.txt
+echo >> logfile.txt
+
+
 while true; do
     echo "$(top -b -n1 | grep -m1 $program_name | awk '{printf "%.2f%%\n", $9}')"
 
