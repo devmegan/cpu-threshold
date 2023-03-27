df33fdde766f684c59aef95f102fd3d62f1740f1
devmegan <devmegan@protonmail.com>
2023-03-27 20:21:48 +0100

    add: echo dynamic logfile link to terminal

--- cpu-monitor.sh
+++ cpu-monitor.sh
@@ -2,7 +2,7 @@
 
 program_name=$1
 
-echo "CPU threshold monitor for $program_name running"
+echo -e "CPU threshold monitor for $program_name running being written to \e]8;;file://$(pwd)/logfile.txt\alogfile.txt\e]8;;\a"
 echo
 
 echo "CPU threshold monitor for $program_name [$(date '+%Y-%m-%d %H:%M:%S')]:" >> logfile.txt
