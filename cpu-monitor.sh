abfa9c82c366c22d8a4b024443a608a54e8294b5
devmegan <devmegan@protonmail.com>
2023-03-27 20:30:49 +0100

    add: write cpu usage to logfile

--- cpu-monitor.sh
+++ cpu-monitor.sh
@@ -2,15 +2,17 @@
 
 program_name=$1
 
-echo -e "CPU threshold monitor for $program_name running being written to \e]8;;file://$(pwd)/logfile.txt\alogfile.txt\e]8;;\a"
+logfile="logfile.txt"
+
+echo -e "CPU threshold monitor for $program_name running being written to \e]8;;file://$(pwd)/$logfile\a$logfile\e]8;;\a"
 echo
 
-echo "CPU threshold monitor for $program_name [$(date '+%Y-%m-%d %H:%M:%S')]:" >> logfile.txt
-echo >> logfile.txt
+echo "CPU threshold monitor for $program_name [$(date '+%Y-%m-%d %H:%M:%S')]:" >> $logfile
+echo >> $logfile
 
 
 while true; do
-    echo "$(top -b -n1 | grep -m1 $program_name | awk '{printf "%.2f%%\n", $9}')"
+    top -b -n 1 | grep -m1 $program_name | awk '{printf "%.2f%%\n", $9}' >> logfile.txt
 
     sleep 60
 done
