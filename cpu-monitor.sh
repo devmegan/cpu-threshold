ba4b0a4c1f734f930067ca2089bc0287f8524c3c
devmegan <devmegan@protonmail.com>
2023-03-28 10:06:05 +0100

    add: log when cpu usage over threshold for sustained duration

--- cpu-monitor.sh
+++ cpu-monitor.sh
@@ -10,14 +10,33 @@ if [ -e "$logfile" ]; then
 fi
 
 echo -e "CPU threshold monitor for $program_name running being written to \e]8;;file://$(pwd)/$logfile\a$logfile\e]8;;\a"
-echo
 
 echo "CPU threshold monitor for $program_name [$(date '+%Y-%m-%d %H:%M:%S')]:" >> $logfile
 echo >> $logfile
 
+count=0
+threshold=100
+over_threshold=false
 
-while true; do
-    top -b -n 1 | grep -m1 $program_name | awk '{printf "%.2f%%\n", $9}' >> logfile.txt
+for (( ; ; )); do
+    cpu_usage=$(top -b -n 1 | grep -m1 $program_name | awk '{printf $9}')
 
-    sleep 60
+    if (( $(echo "$cpu_usage >= $threshold" | bc -l) )); then
+        ((count++))
+
+        if (( $count % 5 == 0 && !$over_threshold )); then
+            echo "[$(date '+%Y-%m-%d %H:%M:%S')] CPU usage over $threshold% for more than $count seconds" >> logfile.txt
+            over_threshold=true
+        fi
+    else
+        count=0
+
+        if $over_threshold; then
+            echo "[$(date '+%Y-%m-%d %H:%M:%S')] CPU usage dropped below 100%" >> logfile.txt
+            echo >> logfile.txt
+            over_threshold=false
+        fi
+    fi
+
+    sleep 1
 done
